2d4f76766597ceff68e1655c8bbdb8be76b56560
sWitched-0ff <azhuravlov@gmail.com>
2013-10-18 00:37:56 +0400

    gotovo

--- js/main.js
+++ js/main.js
@@ -1,3 +1,37 @@
-﻿$(document).ready(function(){
+(function($) {
+	$(function() {
+		$('#partners').jcarousel({
+			circular: true,
+			wrap: 'circular'
+		});
+		$('#partners').jcarouselAutoscroll({
+			'interval': 5000
+		});
 
-});
+		$('#partnersprev')
+			.on('active.jcarouselcontrol', function() {
+				$(this).removeClass('inactive');
+			})
+			.on('inactive.jcarouselcontrol', function() {
+				$(this).addClass('inactive');
+			})
+			.jcarouselControl({
+				target: '-=1'
+			});
+
+		$('#partnersnext')
+			.on('active.jcarouselcontrol', function() {
+				$(this).removeClass('inactive');
+			})
+			.on('inactive.jcarouselcontrol', function() {
+				$(this).addClass('inactive');
+			})
+			.jcarouselControl({
+				target: '+=1'
+			});
+		$('.topNavItem a').click(function(e){
+			e.stopPropagation();
+			$($(this).attr('href')).ScrollTo({offsetTop: 100});
+			});
+	});
+})(jQuery);
